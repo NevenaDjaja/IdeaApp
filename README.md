dd6e7250d460a735fca10345118f0da0c19d1112
Nevena Djaja <nevena.djaja@gmail.com>
2016-11-18 09:16:55 -0800

    added middleware, refactored

---
+++ README.md
@@ -0,0 +1,13 @@
+### Idea app
+======
+An app for storing and sharing ideas.
+
+#### Notes
+
+Clone the app and run
+```
+$ npm install
+$ npm start
+```
+
+Open your browser and type [localhost:3000](http://localhost:3000/) to bring up the app.
---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = require('./lib/middleware.js');
+
+app.use(express.static(__dirname + '/public'));
+app.use(middleware.logger);
+
+app.get('/login', middleware.requireAuthentication, function(req,res) {
+  res.send('Hello Express');
+});
+
+app.listen(PORT, function() {
+  console.log('Server started on port:', PORT);
+});
